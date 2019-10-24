c4d95248268a293b386db919aaec18dc97af2202
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2019-10-24 15:08:56 +0200

    Initial implementation

---
+++ server.js
@@ -0,0 +1,65 @@
+const {readFileSync} = require('fs')
+const express = require('express')
+const cors = require('cors')
+const morgan = require('morgan')
+const Keyv = require('keyv')
+const {safeLoad} = require('js-yaml')
+const HttpResolver = require('./lib/resolvers/http')
+
+const ONE_HOUR = 60 * 60
+const ONE_DAY = 24 * ONE_HOUR
+const EXPIRES_DURATION = 7 * ONE_DAY
+
+const app = express()
+const cache = new Keyv('sqlite://cache.sqlite')
+
+app.disable('x-powered-by')
+app.use(cors())
+
+if (process.env.NODE_ENV !== 'production') {
+  app.use(morgan('dev'))
+}
+
+const tiles = safeLoad(readFileSync('./tiles.yml'))
+
+function w(handler) {
+  return async (req, res, next) => {
+    try {
+      await handler(req, res, next)
+    } catch (error) {
+      next(error)
+    }
+  }
+}
+
+tiles.forEach(tileService => {
+  app.use(`/${tileService.name}/tiles`, (req, res, next) => {
+    res.sendTile = tileData => {
+      res
+        .set('cache-control', `public, max-age=${EXPIRES_DURATION}`)
+        .set('expires', new Date(Date.now() + (EXPIRES_DURATION * 1000)).toUTCString())
+        .type(tileService.imageType)
+        .send(tileData)
+    }
+
+    next()
+  })
+
+  app.get(`/${tileService.name}/tiles/:z/:x/:y`, w(async (req, res) => {
+    const {z, x, y} = req.params
+    const key = `${tileService.name}-${z}-${x}-${y}`
+
+    const cachedTile = await cache.get(key)
+    if (cachedTile) {
+      return res.sendTile(cachedTile.data)
+    }
+
+    const resolver = new HttpResolver(tileService.resolver.url, tileService.resolver.headers || {})
+    const tile = await resolver.getTile(z, x, y)
+    await cache.set(key, tile)
+
+    res.sendTile(tile.data)
+  }))
+})
+
+app.listen(process.env.PORT || 5000)
